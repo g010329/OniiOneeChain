a40b7a3b34d28ee3ea2f2914d6378eb620c3c833
Arron Su <e3e38888@gmail.com>
2024-08-24 14:05:33 +0800

    init project

--- frontend/kage-pay/src/app/_common/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./button/MainButton";
-export * from "./chart/OrganicCircle/OrganicCircle";
-export * from "./card/ListCard";
-export * from "./dialog/Dialog";
--- frontend/kage-pay/src/client.ts
+++
@@ -1,9 +0,0 @@
-import { createPublicClient, createWalletClient, http, custom } from "viem";
-import { sepolia } from "viem/chains";
-
-export const publicClient = createPublicClient({
-  chain: sepolia,
-  transport: http(
-    "https://eth-sepolia.g.alchemy.com/v2/DgoQgIklXGSGCY5-7rekG4CiV6nKO-A6"
-  ),
-});
