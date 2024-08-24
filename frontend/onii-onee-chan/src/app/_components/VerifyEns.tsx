21f017b0d0415efcaf2c377fa9ed972932a2df49
Arron Su <e3e38888@gmail.com>
2024-08-24 19:05:21 +0800

    some animation

--- frontend/onii-onee-chan/src/app/_components/VerifyEns.tsx
+++ frontend/onii-onee-chan/src/app/_components/VerifyEns.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import {
   Card,
   CardContent,
@@ -8,19 +9,31 @@ import {
 } from "@/components/ui/card";
 
 function VerifyEns() {
+  const shakeAnimation = {
+    scale: [1, 1.03, 1.03, 1.03, 1.03, 1],
+    rotate: [0, -2, 2, -2, 2, 0],
+    transition: {
+      duration: 0.8,
+      repeat: Infinity,
+      repeatDelay: 1.2,
+    },
+  };
+
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
-    </Card>
+    <motion.div animate={shakeAnimation}>
+      <Card className="w-[500px]">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </motion.div>
   );
 }
 
